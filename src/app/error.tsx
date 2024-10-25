f9cbc3fcf4bcfdbf69b579a85f9c9cbad63331c9
Punit <punitmistr@gmail.com>
2024-10-25 08:09:20 +0530

    added the auth clerk

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import NextError from "next/error";
 export default function Error({ error }: { error: Error }) {
   return <NextError statusCode={500} title={error.message} />;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,25 @@
 import { Button } from "@/components/ui/button";
+import {
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex flex-col justify-center h-full text-center max-w-5xl mx-auto gap-6 ">
       <h1 className="text-5xl font-bold">Inovicy App</h1>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
       <p>
         <Button>
           <Link href="/dashboard">Sign In</Link>
         </Button>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
       </p>
     </main>
   );
