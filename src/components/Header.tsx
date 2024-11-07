92b597262f02c046889ba0c9a52c3c3b0fcd9eed
Punit <punitmistr@gmail.com>
2024-11-07 13:16:42 +0530

    added the org.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,28 @@
 import React from "react";
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import { ReceiptIndianRupee } from "lucide-react";
+import { OrganizationSwitcher } from "@clerk/nextjs";
 import Link from "next/link";
 import Container from "./Container";
 const Header = () => {
   return (
     <Container className="w-full">
       <header className="flex items-center justify-between py-4 ">
-        <Link
-          href="/"
-          className="font-bold flex gap-3 items-center text-md md:text-3xl capitalize "
-        >
-          <ReceiptIndianRupee />
-          Inovicing App
-        </Link>
+        <div className="flex items-center gap-4">
+          <Link
+            href="/"
+            className="font-bold flex gap-3 items-center text-md  capitalize "
+          >
+            <ReceiptIndianRupee />
+            Inovicing App
+          </Link>
+          /
+          <div>
+            <SignedIn>
+              <OrganizationSwitcher hidePersonal afterCreateOrganizationUrl={"/dashboard"} />
+            </SignedIn>
+          </div>
+        </div>
         <div>
           <SignedOut>
             <SignInButton />
